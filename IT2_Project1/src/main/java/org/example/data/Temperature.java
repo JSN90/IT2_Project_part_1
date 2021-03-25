85c038627cc1ffc57fbd8e5c7ec741ad7d6c520a
Williamg91 <william.geismar@gmail.com>
2021-03-25 14:25:56 +0100

    tepmeratur har nu en værdi man kan få ud

--- IT2_Project1/src/main/java/org/example/data/Temperature.java
+++ IT2_Project1/src/main/java/org/example/data/Temperature.java
@@ -2,8 +2,19 @@ package org.example.data;
 
 import org.example.Controller;
 
+import java.util.Random;
+
 public class Temperature extends Controller {
 
+
+    public Temperature(double max,double min){
+        this.min=min;
+        this.max=max;
+
+    }
+
+    Random temperatur = new Random();
+    double max,min;
     public static void main(String[] args) {
 
         double temperature = Math.random();
@@ -11,5 +22,12 @@ public class Temperature extends Controller {
         System.out.println(temperature);
 
     }
+
+    public double getValue(){
+       double  value = min + (max - min) * temperatur.nextDouble();
+                return value;
+    };
+
+
 
 }
