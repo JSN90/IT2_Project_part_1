4493f48bc99ad0016cdf00a5b600bc974481567c
ZAkir Hussain Shahoo <s194054@student.dtu.dk>
2021-04-30 00:22:13 +0200

    We can now store and call back data into/ from Database:

--- IT2_Project1/src/main/java/org/example/App.java
+++ IT2_Project1/src/main/java/org/example/App.java
@@ -5,22 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.example.sql.DBConnection;
 import org.example.sql.Main;
+import org.example.sql.UseDB;
 
 import java.io.IOException;
+import java.sql.Connection;
 
 public class App extends Application {
 
     private static Scene scene;
-
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("login"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-        //System.out.println("Hej verden");
-
-    }
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
@@ -34,7 +28,29 @@ public class App extends Application {
     public static void main(String[] args) {
         launch();
         //Main from sql pakage
-        Main.main(null);
+        //Main.main(null);
+        // Object of the connection
+        Connection sqlite = DBConnection.sqliteConnection();
+        UseDB dbuser = new UseDB(sqlite);
+
+        for (int i=0;i<30;i++) {
+
+            // You can choos a Patient to be investigated
+            //dbuser.insertIntoDatabase(123456789);
+
+
+        }
+        // You can search for any patients data records by typing her/ his ID:
+        dbuser.findAllMeasurementsFromPatient(5577);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("login"), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+        //System.out.println("Hej verden");
+
     }
 
 
